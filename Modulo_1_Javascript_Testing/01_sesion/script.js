4dd6c5c77e423665cdcad160a23801b00b858fa8
ElliotGomez <126111554+ElliotGomez@users.noreply.github.com>
2025-03-24 10:11:15 -0600

    Add files via upload

---
+++ Modulo_1_Javascript_Testing/01_sesion/script.js
@@ -0,0 +1,6 @@
+var nombre = "Alvaro Elliot";
+var apellido = "Gomez Chavez"
+var nombreCompleto = `${nombre} ${apellido}`
+var edad = 48;
+
+console.log(`¡Hola!, soy ${nombreCompleto} y tengo ${edad} años de edad.`)
