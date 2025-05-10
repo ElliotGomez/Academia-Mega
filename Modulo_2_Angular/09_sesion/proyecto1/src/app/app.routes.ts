1f48f78c2b484e0c2865bde06f58e5b0122c6fef
ElliotGomez <126111554+ElliotGomez@users.noreply.github.com>
2025-05-10 12:31:42 -0600

    Comunicación entre @InPut() y @OutPut()

---
+++ Modulo_2_Angular/09_sesion/proyecto1/src/app/app.routes.ts
@@ -0,0 +1,29 @@
+import { Routes } from '@angular/router';
+import { ProductManagerComponent } from './Components/product-manager/product-manager.component';
+import { TodoComponent } from './Components/todo/todo.component';
+import { HomeComponent } from './Page/home/home.component';
+import { ErrorComponent } from './Page/error/error.component';
+import { ProductosInfoComponent } from './Page/productos-info/productos-info.component';
+
+export const routes: Routes = [
+  {
+    path:'',
+    component: ProductosInfoComponent
+  },
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path:'gestor',
+    component: ProductManagerComponent
+  },
+  {
+    path:'todo',
+    component: TodoComponent
+  },
+  {
+    path:'**',
+    component: ErrorComponent
+  }
+];
