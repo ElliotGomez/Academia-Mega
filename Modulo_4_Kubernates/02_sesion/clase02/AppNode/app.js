3818b2e0c207c54b625ade7eba59b43ac2f6492d
ElliotGomez <126111554+ElliotGomez@users.noreply.github.com>
2025-08-21 21:06:26 -0600

    Clase02

---
+++ Modulo_4_Kubernates/02_sesion/clase02/AppNode/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.PORT || 3000;
+
+
+app.get('/' , (req, rest) => {
+    rest.send("Hola mundo les dice Adonai Rios Rodríguez, prueba con volumenes");
+});
+
+app.listen(PORT , ()=>
+{
+    console.log(`Servidor de node escuchando el puerto ${PORT}`)
+});
+
